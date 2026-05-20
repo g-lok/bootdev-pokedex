a6e243bf95fb72ef68498aa34eae255231370fb4
G <g@devnull.rip>
2026-05-19 20:03:21 -0700

    l4-repl

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Print("Hello, World!")
+	prompt := "Pokedex > "
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("input error: %v", err)
+		}
+		input := scanner.Text()
+		formatted := cleanInput(input)
+		if len(formatted) == 0 {
+			fmt.Println("no input received")
+		} else {
+			output := fmt.Sprintf("Your command was: %s", formatted[0])
+			fmt.Println(output)
+		}
+	}
 }
